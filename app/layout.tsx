a417eef0ecdb34e1bd8ff37228ab14e506d1da1f
Boniface Michuki <dxmichuki@yahoo.com>
2024-10-24 23:03:27 +0300

    Updated Image model schema and interface handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,20 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Sans } from "next/font/google";
+import { cn } from "@/lib/utils";
 
-import { IBM_Plex_Sans } from 'next/font/google';
 import "./globals.css";
-import {cn} from "@/lib/utils"
-import { ClerkProvider } from '@clerk/nextjs';
+import { ClerkProvider } from "@clerk/nextjs";
 
-
-
-const ibmPlexSans = IBM_Plex_Sans({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-  variable: '--font-ibm-plex-sans'
+const IBMPlex = IBM_Plex_Sans({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  variable: "--font-ibm-plex",
 });
-// Example metadata
-export const metadata = {
-  title: 'IMImaginify',
-  description: 'An AI-powered image generator'
-};
 
+export const metadata: Metadata = {
+  title: 'IMmaginify',
+  description: 'AI-powered image generator',
+};
 
 export default function RootLayout({
   children,
@@ -24,13 +22,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-   <ClerkProvider>
-    <html lang="en">
-      <body className={cn("font-IBMPlex antialiased", ibmPlexSans.variable)}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          {children}
+        </body>
+      </html>
     </ClerkProvider>
-  
   );
 }
+
