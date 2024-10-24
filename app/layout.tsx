ec3e470936675c175e5a7f16ddcc8c2c3a9a6472
Boniface Michuki <dxmichuki@yahoo.com>
2024-10-24 22:39:28 +0300

    Updated Image model schema and interface handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import { IBM_Plex_Sans } from 'next/font/google';
 import "./globals.css";
 import {cn} from "@/lib/utils"
+import { ClerkProvider } from '@clerk/nextjs';
 
 
 
@@ -23,13 +24,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-   
+   <ClerkProvider>
     <html lang="en">
       <body className={cn("font-IBMPlex antialiased", ibmPlexSans.variable)}>
         {children}
       </body>
     </html>
-
+    </ClerkProvider>
   
   );
 }
